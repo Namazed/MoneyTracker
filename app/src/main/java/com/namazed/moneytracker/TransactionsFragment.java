3d87d9465f5fa6f8bf2c66c1561fdd8ebd55ac6b
Namazed <actek17@gmail.com>
2016-07-04 21:24:33 +0700

    Added Floating Action Button in Transactions (layout and class).

--- app/src/main/java/com/namazed/moneytracker/TransactionsFragment.java
+++ app/src/main/java/com/namazed/moneytracker/TransactionsFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.melnykov.fab.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -30,14 +32,15 @@ public class TransactionsFragment extends Fragment {
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.transactions_list);
         //FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
-        //mRecyclerView.setHasFixedSize(true); //Только для телефонов, фиксированный размер.
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        mRecyclerView.setHasFixedSize(true); //Только для телефонов, фиксированный размер.
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         //Сохраняем в менеджере вертикальную ориентацию
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         //присвоили view ориентацию, с помощью manager
         mRecyclerView.setLayoutManager(linearLayoutManager);
         mRecyclerView.setAdapter(mTransactionAdapter);
-        //fab.attachToRecyclerView(mRecyclerView);
+        fab.attachToRecyclerView(mRecyclerView);
         return view;
 //        return super.onCreateView(inflater, container, savedInstanceState);
     }
@@ -47,6 +50,15 @@ public class TransactionsFragment extends Fragment {
         data.add(new Transaction("T-Shirts", 1000, new Date().toString()));
         data.add(new Transaction("Jeans", 3000, new Date().toString()));
 
+        data.add(new Transaction("Asfsd", 100, new Date().toString()));
+        data.add(new Transaction("Suah", 10, new Date().toString()));
+        data.add(new Transaction("Jns", 5000, new Date().toString()));
+        data.add(new Transaction("Tl", 2000, new Date().toString()));
+        data.add(new Transaction("Thts", 1, new Date().toString()));
+        data.add(new Transaction("Jeans", 600, new Date().toString()));
+        data.add(new Transaction("sfdTl", 2065, new Date().toString()));
+        data.add(new Transaction("fts", 98, new Date().toString()));
+        data.add(new Transaction("Jaa", 6070, new Date().toString()));
         return data;
     }
 }
