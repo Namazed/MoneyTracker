d0064e7e967de300a62234112465dadf4e16d582
Namazed <actek17@gmail.com>
2016-07-06 19:17:22 +0700

    Added Activity and Layout (add transaction)

---
+++ app/src/main/java/com/namazed/moneytracker/AddTransactionActivity.java
@@ -0,0 +1,43 @@
+package com.namazed.moneytracker;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class AddTransactionActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+
+        setTitle(getString(R.string.add_transaction));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+//                 такой возврат только при большом количестве Activity в приложении, возврат домой
+//                NavUtils.navigateUpFromSameTask(this);
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
